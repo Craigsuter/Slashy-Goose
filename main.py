99db5d151c0e92d5c3fe2671f3b5b9120b32bb6b
Craigsuter <willcrane78@gmail.com>
2023-04-25 09:24:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -114,7 +114,7 @@ class aclient(discord.Client):
     scheduler = AsyncIOScheduler()
     #Post on the day of a game
     try:
-        #await testingspam()
+        await testingspam()
         scheduler.add_job(testingspam, CronTrigger(minute="15, 45"))
         scheduler.add_job(testingtundraspam, CronTrigger(minute="5, 10, 15, 25, 35, 45, 55"))
         scheduler.add_job(testingspamsentinels, CronTrigger(minute="10, 20, 30, 40, 50, 0"))

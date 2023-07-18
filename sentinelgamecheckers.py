9cea392fb4efd139a7068fc22e6b3ab846b5979e
Craigsuter <willcrane78@gmail.com>
2023-07-18 17:31:25 +0000

    adding string text

--- sentinelgamecheckers.py
+++ sentinelgamecheckers.py
@@ -182,7 +182,7 @@ def SentinelsValoCheck(channelDataID, pageURL, isShort):
     minuteofgame = UTCTime2[1]
     dt_string_year = "20" + str(dt_string_year)
     a = datetime.datetime(int(yearofgame), int(monthnumber), int(dayofgame2), (int(hourofvalo)) , int(minuteofgame), 0)
-    if(timecheckingdevice == 12 and prefixOfTime =="am"):
+    if(str(timecheckingdevice) == "12" and prefixOfTime =="am"):
       a = a.timedelta(-39600)
     b = datetime.datetime(int(dt_string_year), int(dt_string_month), int(dt_string_day), int(dt_string_hour), int(dt_string_minute), int(dt_string_second))
 
